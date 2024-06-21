5cc99086d19da266dce4546af4bd28145028a32e
Rebecca Lang <rebeccalang50@gmail.com>
2024-06-21 17:58:40 +1200

    examples for functions added

---
+++ images-blog/codesnippets.js
@@ -0,0 +1,15 @@
+function bananaBread() {
+  let result = 'I love banana bread!'
+  console.log(result)
+}
+
+bananaBread()
+console.log(bananaBread)
+
+let banana = 'Banana'
+let bread = 'bread'
+
+function add(banana, bread) {
+  let result = 'banana' + 'bread' + 'is yummy!'
+  console.log(result)
+}
